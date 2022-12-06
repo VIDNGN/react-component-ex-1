b14ebf2b6c05ae630069938e6d1954adb4f21e44
Vi <vi@justvi.com>
2022-12-06 12:31:25 -0800

    Initial commit

---
+++ src/FavList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FavList() {
+  return (
+    <ul>
+      <li>coffee</li>
+      <li>tennis</li>
+      <li>sun</li>
+    </ul>
+  );
+}
+
+export default FavList;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FavList from "./FavList";
+import Heading from "./Heading";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <FavList />
+  </div>,
+  document.getElementById("root")
+);
